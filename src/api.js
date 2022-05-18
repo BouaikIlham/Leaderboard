857cbecffd0f67128f51b2932a68f0a69f36c31a
ilham bouaik <ibouaik@gmail.com>
2022-05-18 22:59:10 +0100

    Fix linter errors

--- src/api.js
+++ src/api.js
@@ -1,28 +1,28 @@
-import axios from "axios";
-const gameId = 'HNOxjEhF0ya84VTJccpZ'
+import axios from 'axios';
 
+const gameId = 'HNOxjEhF0ya84VTJccpZ';
 
 const getScores = async () => {
-    let result
-    await axios.get(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`)
-        .then((response) => {
-            result = response.data.result
-        })
-        .catch((error) => {
-            console.log(error);
-        })
+  let result;
+  await axios.get(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`)
+    .then((response) => {
+      result = response.data.result;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 
-    return result
-}
+  return result;
+};
 
 const createScore = async (user, score) => {
-    await axios.post(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`, { user: user, score: score })
-        .then(response => {
-            console.log(response)
-        })
-        .catch(error => {
-            console.log(error)
-        })
-}
+  await axios.post(`https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`, { user, score })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
 
-export { getScores, createScore }
+export { getScores, createScore };
--- src/index.js
+++ src/index.js
@@ -1,35 +1,34 @@
-import { getScores, createScore } from './api.js'
-const listOfScores = document.querySelector('.scores')
-const user = document.getElementById('name')
-const score = document.getElementById('score')
+import { getScores, createScore } from './api.js';
 
+const listOfScores = document.querySelector('.scores');
+const user = document.getElementById('name');
+const score = document.getElementById('score');
 
 const showScores = (scores) => {
-    listOfScores.textContent = ''
-    scores.map(score => {
-        const list = document.createElement('li')
-        list.className = ('list')
-        list.textContent = score.user + ":" + score.score
-        listOfScores.appendChild(list)
-    })
-}
-
-const refrechBtn = document.querySelector('.refrech-btn')
-
-refrechBtn.addEventListener('click', async () => {
-    const list = await getScores()
-    showScores(list)
-})
-
-
-window.onload = async () => {
-    const list = await getScores()
-    showScores(list);
+  listOfScores.textContent = '';
+  scores.forEach((score) => {
+    const list = document.createElement('li');
+    list.className = ('list');
+    list.textContent = `${score.user}:${score.score}`;
+    listOfScores.appendChild(list);
+  });
 };
 
-const form = document.getElementById('form')
+const refrechBtn = document.querySelector('.refrech-btn');
+
+refrechBtn.addEventListener('click', async () => {
+  const list = await getScores();
+  showScores(list);
+});
+
+window.onload = async () => {
+  const list = await getScores();
+  showScores(list);
+};
+
+const form = document.getElementById('form');
 form.addEventListener('submit', async (e) => {
-    e.preventDefault()
-    await createScore(user.value , score.value)
-    form.reset()
-})
+  e.preventDefault();
+  await createScore(user.value, score.value);
+  form.reset();
+});
